acc4aa77ccf407341513e6659d0ff979d8f2dadc
Bruno Sales <me@baliestri.dev>
2023-05-01 20:09:54 -0300

    feat(envs): add `Hooks` PowerShell module

---
+++ envs/windows/hooks/Hooks.psm1
@@ -0,0 +1,28 @@
+# Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
+# See the LICENSE file in the project root for full license information.
+
+. $PSScriptRoot\Functions.ps1
+
+<#
+.SYNOPSIS
+  Adds a hook to be executed when the current directory changes.
+
+.DESCRIPTION
+  Adds a hook to be executed when the current directory changes.
+
+.PARAMETER ScriptBlock
+  The script block to be executed when the current directory changes.
+
+
+#>
+function Add-ChangeCurrentDirectoryHook {
+  param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullOrEmpty()]
+    [scriptblock]$Hook
+  )
+
+  end {
+    $Script:ChangeCurrentDirectoryHooks += $Hook
+  }
+}
